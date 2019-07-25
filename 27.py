6d01a7e3ff0ab36ca5a2ba441b2e7872ba011f80
Unown256 <39966795+Unown256@users.noreply.github.com>
2019-07-25 18:42:08 +0100

    Deleted 27.py, use Exurb1a27.py instead

--- 27.py
+++
@@ -1,9 +0,0 @@
-import random
-import time
-
-a = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
-b = 1
-while True:
-    print(b," : ",a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)],a[random.randint(0,15)])
-    b += 1
-    time.sleep(0.06)
